e0fecd8b6ab2f9e61a7954343560b352ce2bde12
EmilyCook321 <emilyacook16@gmail.com>
2020-08-05 08:48:48 +0200

    Add toggle button

---
+++ main.js
@@ -0,0 +1,14 @@
+const toggleButton = document.querySelector(".toggle");
+const rootStyle = document.documentElement.style;
+
+let isDark = true;
+toggleButton.addEventListener("click", () => {
+  if (isDark) {
+    rootStyle.setProperty("--main-bg-color", "#fff");
+    rootStyle.setProperty("--main-color", "#000");
+  } else {
+    rootStyle.setProperty("--main-bg-color", "#000");
+    rootStyle.setProperty("--main-bg-color", "#fff");
+  }
+  isDark = !isDark;
+});
