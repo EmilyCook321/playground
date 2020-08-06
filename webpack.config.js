0441e4a5cd0f47ab7a54fabde284409024a847b5
EmilyCook321 <emilyacook16@gmail.com>
2020-08-06 11:09:51 +0200

    Add image loading and api

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
     ],
   },
 };
